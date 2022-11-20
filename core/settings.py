6d59f78fc6e375f0d0e2de1a8820b54f0d3ccd5d
Alberto Senna Dias Neto <albertosdneto@gmail.com>
2022-11-20 02:36:08 -0300

    Update settings.py
    update static files config

--- core/settings.py
+++ core/settings.py
@@ -127,6 +127,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
@@ -135,4 +137,3 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 OPEN_WEATHER_API_KEY = config('OPEN_WEATHER_API_KEY', default=None)
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
